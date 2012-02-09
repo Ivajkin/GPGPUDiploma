71e4b1f0959dad69483251fb86e2e6d71c33bf8e
Ivajkin Timofej <timatnet@gmail.com>
2012-02-09 20:21:57 +1100

    OpenCL

--- OpenGLGalaxy/OpenGLWater/Logic.cpp
+++ OpenGLGalaxy/OpenGLWater/Logic.cpp
@@ -27,7 +27,6 @@ void Logic::createStar() {
 
 void Logic::update( /* seconds */ double elapsedTime) {
 
-	//processGPGPU~~
 	static AutoProcessProfiler gravityComputationProcess("Gravity Computation Process");
 
 	// cudaDeviceSynchronize()
@@ -40,6 +39,60 @@ void Logic::update( /* seconds */ double elapsedTime) {
 void Logic::initGPGPU() {
 #ifdef CUDA_GPGPU
 #elif defined OPENCL_GPGPU
+	// Коды ошибок.
+	cl_int error = 0;
+	// Платформа
+	error = oclGetPlatformID(&platform);
+	if (error != CL_SUCCESS) {
+	   cout << "Ошика получения id платформы: " << errorMessage(error) << endl;
+	   exit(error);
+	}
+	// Устройство
+	error = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, NULL);
+	if (err != CL_SUCCESS) {
+	   cout << "Ошибка получения id устройств: " << errorMessage(error) << endl;
+	   exit(error);
+	}
+	// Контекст
+	context = clCreateContext(0, 1, &device, NULL, NULL, &error);
+	if (error != CL_SUCCESS) {
+	   cout << "Ошибка создания контекста: " << errorMessage(error) << endl;
+	   exit(error);
+	}
+	// Очередь команд
+	queue = clCreateCommandQueue(context, device, 0, &error);
+	if (error != CL_SUCCESS) {
+	   cout << "Ошибка создания очереди команд: " << errorMessage(error) << endl;
+	   exit(error);
+	}
+
+	// Создаём программу.
+	// Вспомогательные функции NVIDIA для получения содержимого файла и его размера.
+	size_t src_size = 0;
+	const char* path = shrFindFilePath("vector_add_gpu.cl", NULL);
+	const char* source = oclLoadProgSource(path, "", &src_size);
+	cl_program program = clCreateProgramWithSource(context, 1, &source, &src_size, &error);
+	assert(error == CL_SUCCESS && "Не получилось создать программу");
+
+	// Билдим программу.
+	error = clBuildProgram(program, 1, &device, NULL, NULL, NULL);
+	assert(error == CL_SUCCESS && "Не скомпилировалось");
+
+	// Лог.
+	char* build_log;
+	size_t log_size;
+	// Пробный вызов, для того чтобы узнать размер.
+	clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
+	build_log = new char[log_size+1];
+	// Получаем теперь лог.
+	clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, log_size, build_log, NULL);
+	build_log[log_size] = '\0';
+	cout << build_log << endl;
+	delete[] build_log;
+
+	// Получаем kernel (программу на видеокарте (функцию)).
+	kernel = clCreateKernel(program, "evaluate_gravity", &error);
+	assert(error == CL_SUCCESS && "Не смог извлечь kernel");
 #elif defined CPU_GPGPU
 #else
 	assert(false && "There is no valid processor type defined (CUDA_GPGPU, OPENCL_GPGPU or CPU_GPGPU)");
@@ -47,9 +100,32 @@ void Logic::initGPGPU() {
 }
 
 // 
-void Logic::processGPGPU(double elapsedTime, point* points, const int point_count) {
+void Logic::processGPGPU(double elapsedTime, point* points, const size_t point_count) {
 #ifdef CUDA_GPGPU
 #elif defined OPENCL_GPGPU
+	const int mem_size = sizeof(point)*point_count;
+	// Выделяем буффер и копируем в него.
+	cl_mem src_d = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, mem_size, src_a_h, &error);
+	cl_mem res_d = clCreateBuffer(context, CL_MEM_WRITE_ONLY, mem_size, NULL, &error);
+	
+	// Собираем параметры, передаём их размер, а не размер того, на что они указывают.
+	error = clSetKernelArg(kernel, 0, sizeof(double), &elapsedTime);
+	error |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &src_d);
+	error |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &res_d);
+	error |= clSetKernelArg(kernel, 3, sizeof(size_t), &point_count);
+	assert(error == CL_SUCCESS && "Не получилось передать аргументы");
+
+	// Запускаем kernel
+	// Количество единиц работы на рабочую группу
+	const size_t local_ws = 512;
+	// shrRoundUp возвращает наименьшее число, большее чем размер, являющееся произведением local_ws на целое число.
+	const size_t global_ws = shrRoundUp(local_ws, size);
+	// Кол-во элементов работы.
+	error = clEnqueueNDRangeKernel(queue, vector_add_k, 1, NULL, &global_ws, &local_ws, 0, NULL, NULL);
+	assert(error == CL_SUCCESS && "Не получилось создать очередь работы");
+
+	// Получаем результат, записываем обратно в points.
+	clEnqueueReadBuffer(queue, res_d, CL_TRUE, 0, mem_size, points, 0, NULL, NULL);
 #elif defined CPU_GPGPU
 	for(int i = 0; i < point_count; ++i) {
 		for(int j = 0; j < point_count; ++j) {
@@ -92,3 +168,11 @@ void Logic::disposeGPGPU() {
 	assert(false && "There is no valid processor type defined (CUDA_GPGPU, OPENCL_GPGPU or CPU_GPGPU)");
 #endif
 }
+
+#ifdef OPENCL_GPGPU
+cl_platform_id Logic::platform;
+cl_context Logic::context;
+cl_command_queue Logic::queue;
+cl_device_id Logic::device;
+cl_kernel Logic::kernel;
+#endif
--- OpenGLGalaxy/OpenGLWater/Logic.h
+++ OpenGLGalaxy/OpenGLWater/Logic.h
@@ -2,6 +2,14 @@
 #include "Star.h"
 #include <vector>
 
+// All OpenCL headers
+#if defined (__APPLE__) || defined(MACOSX)
+    #include <OpenCL/opencl.h>
+#else
+    #include <CL/opencl.h>
+#endif 
+
+
 class Logic {
 public:
 	Logic();
@@ -9,10 +17,18 @@ public:
 	void createStar();
 	void update( /* seconds */ double elapsedTime);
 	static void initGPGPU();
-	static void processGPGPU(double elapsedTime, point* points, const int point_count);
+	static void processGPGPU(double elapsedTime, point* points, const size_t point_count);
 	static void disposeGPGPU();
 private:
 	std::vector<Star*> starField;
 	static const int MAX_POINT_COUNT = 4096;
 	point* pointField;
+
+#ifdef OPENCL_GPGPU
+	static cl_platform_id platform;
+	static cl_context context;
+	static cl_command_queue queue;
+	static cl_device_id device;
+	static cl_kernel kernel;
+#endif
 };
